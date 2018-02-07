8f4a0bef32b3315f34d147ab5558613dac47fdd8
Fabian Will <fabian.will@chamaeleon.de>
2018-02-07 08:19:21 +0100

    first qommit

---
+++ src/main/java/beans/HelloAngularBeans.java
@@ -0,0 +1,15 @@
+package beans;
+
+import angularBeans.api.AngularBean;
+import angularBeans.api.NGReturn;
+import angularBeans.api.http.Get;
+
+@AngularBean
+public class HelloAngularBeans {
+    @Get
+    @NGReturn(model = "message")
+    public String sayHello(String name) {
+        return "hello " + name + " from AngularBeans !";
+
+    }
+}
---
+++ src/main/resources/js/controllers.js
@@ -0,0 +1,5 @@
+function MyCtrl(helloAngularBeans){
+    var vm=this;
+
+    vm.helloAngularBeans=helloAngularBeans;
+}
